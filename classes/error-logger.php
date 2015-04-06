272c5dabfcdeb617fbb1b820e87ea56130df5e67
Kyle B. Johnson <kyle@wpninjas.com>
2015-04-06 14:32:13 -0400

    add a stub class for the error logger

---
+++ classes/error-logger.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * Handles custom error logging for Ninja Forms and Ninja Forms Extensions.
+ *
+ * @package     Ninja Forms
+ * @subpackage  Classes/Errors
+ * @copyright   Copyright (c) 2015, WPNINJAS
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       2.9.8
+ */
+
+class NF_Error_Logger {
+
+}
