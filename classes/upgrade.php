ec586155eb7ef929367391c25fe4f6ed40f0ca21
Kyle B. Johnson <kyle@wpninjas.com>
2015-04-10 17:03:23 -0400

    add static getter method

--- classes/upgrade.php
+++ classes/upgrade.php
@@ -5,4 +5,31 @@
  */
 class NF_Upgrade {
 
+    protected $id;
+
+    public function __construct( $id ) {
+        $this->id;
+    }
+
+    /**
+     * Upgrades
+     *
+     * Return an array of NF_Upgrade objects
+     *
+     * @return array
+     */
+    public static function upgrades() {
+        global $wpdb;
+
+        $results = $wpdb->get_results( "SELECT * FROM " . NF_OBJECTS_TABLE_NAME . " WHERE type = '" . NF_Upgrade_Handler::OBJECT_TYPE . "'", ARRAY_A );
+
+        $upgrades = array();
+
+        foreach ( $results as $result ) {
+            $upgrades[] = new NF_Upgrade( $result['id'] );
+        }
+
+        return $upgrades;
+    }
+
 }
