c3df5ed130b5ca76c069ef093d700f4980c03b24
Kyle B. Johnson <kyle@wpninjas.com>
2015-04-10 15:13:17 -0400

    add upgrade and upgrade handler to classes folder

---
+++ classes/upgrade-handler.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Class NF_Upgrade_Handler
+ */
+class NF_Upgrade_Handler {
+
+}
---
+++ classes/upgrade.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Class NF_Upgrade
+ */
+class NF_Upgrade {
+
+}
