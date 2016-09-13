03bc37c93151f61c87416d673bb4e94e0d482de8
Reza Heidari <reza@wearerivet.com>
2016-09-12 20:37:18 -0500

    changing selector typo (should be ID instead of class). publishing built files

--- assets/js/front-end/controllers/actionSuccess.js
+++ assets/js/front-end/controllers/actionSuccess.js
@@ -7,7 +7,7 @@ define([], function() {
 		actionSubmit: function( response ) {
 			if ( _.size( response.errors ) == 0 && 'undefined' != typeof response.data.actions ) {
 				if ( 'undefined' != typeof response.data.actions.success_message && '' != response.data.actions.success_message ) {
-					jQuery( '.nf-form-'+response.data.form_id+'-cont .nf-response-msg' ).html( response.data.actions.success_message );
+					jQuery( '#nf-form-'+response.data.form_id+'-cont .nf-response-msg' ).html( response.data.actions.success_message );
 				}
 			}
 		}
